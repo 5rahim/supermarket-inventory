95fa590e75840bbbb5f305f6c501b29e49d03f74
5rahim <bonfohzaki@gmail.com>
2023-04-25 14:15:53 -0400

    list suppliers

--- src/pages/supermarket/suppliers.tsx
+++ src/pages/supermarket/suppliers.tsx
@@ -1,21 +1,24 @@
 import { ProtectedPage } from '@/components/auth/ProtectedPage'
-import DebugData from '@/components/DebugData'
 import { Layout } from '@/components/layout/Layout'
 import { useDisclosure } from '@/hooks/use-disclosure'
 import { useSupermarket } from '@/hooks/use-supermarket'
 import { getServerAuthSession } from '@/server/auth'
 import { InferType } from '@/types'
 import { api } from '@/utils/api'
+import { Supplier } from '@prisma/client'
+import { BiEditAlt } from '@react-icons/all-files/bi/BiEditAlt'
+import { ColumnDef } from '@tanstack/react-table'
 import { Button } from '@ui/main/forms/button/Button'
 import { createTypesafeFormSchema } from '@ui/main/forms/typesafe-form/CreateTypesafeFormSchema'
 import { Field } from '@ui/main/forms/typesafe-form/Field'
 import { TypesafeForm } from '@ui/main/forms/typesafe-form/TypesafeForm'
 import { PageHeader } from '@ui/main/layout/page-header/PageHeader'
 import { Modal } from '@ui/main/overlay/modal/Modal'
+import { DataGrid } from '@ui/shared/data-grid/DataGrid'
 import { LoadingSpinner } from '@ui/shared/loading-spinner/LoadingSpinner'
 import { GetServerSideProps, NextPage } from 'next'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { v4 } from 'uuid'
 
 export const supplierSchema = createTypesafeFormSchema(({ z, presets }) => z.object({
@@ -32,6 +35,30 @@ const Page: NextPage = () => {
    const { supermarket, isLoading, isEmpty } = useSupermarket()
    const supplierQuery = api.supplier.getAll.useQuery({ supermarketId: supermarket?.id }, { enabled: !!supermarket })
    
+   const columns = useMemo<ColumnDef<Supplier>[]>(() => {
+      return [
+         {
+            accessorKey: 'name',
+            header: 'Name',
+            cell: info => <a
+               className="flex items-center gap-4"
+            >
+               <span className="text-xl font-semibold flex-none">{info.getValue() as string}</span>
+               <BiEditAlt className="text-lg" />
+            </a>,
+            size: 100,
+            footer: props => props.column.id,
+         },
+         {
+            accessorKey: 'email',
+            header: () => 'Email',
+            cell: info => info.getValue(),
+            footer: props => props.column.id,
+            size: 90,
+         },
+      ]
+   }, [supplierQuery.data])
+   
    if (isLoading) return <LoadingSpinner />
    if (isEmpty) router.push('/supermarket')
    if (supermarket) {
@@ -43,24 +70,19 @@ const Page: NextPage = () => {
                   <Button onClick={creationModal.open}>Add a supplier</Button>
                </>}
                />
-               {/*<DataGrid<TableOrders>*/}
-               {/*   columns={columns}*/}
-               {/*   data={data}*/}
-               {/*   dataCount={data?.length ?? 0}*/}
-               {/*   isLoading={isLoading}*/}
-               {/*   isFetching={false}*/}
-               {/*   itemsPerPage={15}*/}
-               {/*   hideColumns={[*/}
-               {/*      { below: 600, hide: ['total'] },*/}
-               {/*      { below: 500, hide: ['created_at'] },*/}
-               {/*      { below: 400, hide: ['table_id'] },*/}
-               {/*   ]}*/}
-               {/*   enableRowSelection={false}*/}
-               {/*   onItemSelected={data => {*/}
-               {/*      console.log(data)*/}
-               {/*   }}*/}
-               {/*/>*/}
-               <DebugData data={supplierQuery.data} />
+               <DataGrid<Supplier[]>
+                  columns={columns}
+                  data={supplierQuery.data}
+                  dataCount={supplierQuery.data?.length ?? 0}
+                  isLoading={isLoading}
+                  isFetching={false}
+                  itemsPerPage={15}
+                  enableRowSelection={false}
+                  onItemSelected={data => {
+                     console.log(data)
+                  }}
+               />
+               {/*<DebugData data={supplierQuery.data} />*/}
             </Layout>
             
             <Modal title="Add a supplier" isOpen={creationModal.isOpen} onClose={creationModal.close} size="xl" actions={[{ action: 'close' }]}>
